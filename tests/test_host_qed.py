440be11f6fa9977870cd9cfc14cd5ab7859bf0c5
Jeff Minucci <jminucci@uga.edu>
2018-06-22 14:19:52 -0400

    update qed host testing

--- tests/test_host_qed.py
+++ tests/test_host_qed.py
@@ -6,13 +6,14 @@ import mechanicalsoup # for populating and submitting input data forms
 import unicodedata
 from tabulate import tabulate
 from . import linkcheck_helper
+from . import smoketest_secrets
 import numpy as np
 import json
 
 
-pub_server = "http://qed.epa.gov/pram/"
-internal_server_1 = "http://qedinternal.epa.gov/pram/"
-internal_server_5 = "http://134.67.114.5/pram/"
+pub_server = "https://qed.epa.gov/"
+internal_server_1 = "https://qedinternal.epa.gov/"
+internal_server_5 = "http://134.67.114.5/"
 
 
 servers = [pub_server, internal_server_1, internal_server_5]
@@ -23,43 +24,44 @@ models = ["agdrift/", "beerex/", "iec/", "sip/", "stir/", 'terrplant/', 'therps/
 
 models_nonbeta = ["agdrift/", "beerex/", "iec/", "sip/", "stir/", 'terrplant/', 'therps/', 'trex/',
           'kabam/', 'rice/']
-#The following list represents the model page titles to be checked (order of models
-#needs to be the same as "models" list above)
+
 
 models_IO = ["AgDrift", "Bee-REX", "IEC", "SIP", "STIR", "TerrPlant", "T-HERPS", 'T-REX 1.5.2',
                  "KABAM", "RICE", "Agdisp", "Earthworm", "Insect", "Pat", "Perfum", "Pfam",
                  "PWC", "SAM", "TED", "VarroaPop"]
                             
-models_IO_nonbeta = ["AgDrift", "Bee-REX", "IEC", "SIP", "STIR", "TerrPlant", "T-HERPS", 'T-REX 1.5.2',
-                 "KABAM", "RICE"]
+
+non_pram_modules = ["cts/", "hms/", 'pisces/', 'pisces/watershed/', 'pisces/stream/', 'pisces/species/', 'pisces/algorithms/',
+                    'pisces/references/','hwbi/', 'wqt/', 'hem/', 'cyan/']
 
 
 pages = ["","input", "algorithms", "references"] #for now leaving out "qaqc" page
 
-#redirect servers are those where user login fthe input page is required
-redirect_servers = [pub_server]
-redirect_pages = ["input"]
 
-#following are lists of url's to be processed with tests below
-model_pages = [s + m + p for s in servers for m in models for p in pages]
-redirect_model_pages = [s + m + p for s in redirect_servers for m in models
-                        for p in redirect_pages]
-redirect_models = models_IO * len(redirect_servers)
+#all pram models
+pub_model_pages_pram = [pub_server + 'pram/' + m + p for m in models for p in pages]
+s1_model_pages_pram = [internal_server_1  + 'pram/' + m + p for m in models for p in pages]
+s5_model_pages_pram = [internal_server_5  + 'pram/' + m + p for m in models for p in pages]
 
-#all models
-pub_model_pages = [pub_server + m + p for m in models for p in pages]
-s1_model_pages = [internal_server_1 + m + p for m in models for p in pages]
-s5_model_pages = [internal_server_5 + m + p for m in models for p in pages]
 
-#non-beta only
-pub_model_pages_nonbeta = [pub_server + m + p for m in models_nonbeta for p in pages]
-s1_model_pages_nonbeta = [internal_server_1 + m + p for m in models_nonbeta for p in pages]
-s5_model_pages_nonbeta = [internal_server_5 + m + p for m in models_nonbeta for p in pages]
+#non-beta pram only
+pub_model_pages_pram_nonbeta = [pub_server  + 'pram/'+ m + p for m in models_nonbeta for p in pages]
+s1_model_pages_pram_nonbeta = [internal_server_1  + 'pram/'+ m + p for m in models_nonbeta for p in pages]
+s5_model_pages_pram_nonbeta = [internal_server_5 + 'pram/' + m + p for m in models_nonbeta for p in pages]
 
-#hooks for the qed_smoketests Slack app
-pub_server_hook = 'https://hooks.slack.com/services/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'
-s1_hook = 'https://hooks.slack.com/services/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'
-s5_hook = 'https://hooks.slack.com/services/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'
+
+#non-pram modules
+pub_model_pages_other = [pub_server + m for m in non_pram_modules]
+s1_model_pages_other = [internal_server_1  + m for m in non_pram_modules]
+s5_model_pages_other= [internal_server_5  + m for m in non_pram_modules]
+
+#combine pram pages and the base-level non-pram modules
+pub_model_pages = pub_model_pages_pram + pub_model_pages_other
+s1_model_pages = s1_model_pages_pram + s1_model_pages_other
+s5_model_pages = s5_model_pages_pram + s5_model_pages_other
+
+
+
 
 class TestQEDHost(unittest.TestCase):
     """
@@ -74,20 +76,64 @@ class TestQEDHost(unittest.TestCase):
 
     def teardown(self):
         pass
-    
-    def send_slack_message(self,message, hook_url):
-       body = {"text": message}
-       url = hook_url
-       response = requests.post(url, data=json.dumps(body), headers = {'Content-type': 'application/json'})
-       return
+
+    def send_slack_message(self,message, hook_url, server = None):
+        pages_down = message.count('\n') + 1
+        if pages_down > 2:
+            if server is not None:
+                body = {"text": "There are *" + str(pages_down) + "* pages down on " + server,
+                        "attachments": [
+                            {
+                                "text": message
+                            }
+                        ]
+                        }
+            else:
+                body = {"text": "There are " + str(pages_down) + " pages down.",
+                        "attachments": [
+                            {
+                                "text": message
+                            }
+                        ]
+                        }
+        else:
+            body = {"text": message}
+        url = hook_url
+        response = requests.post(url, data=json.dumps(body), headers = {'Content-type': 'application/json'})
+        return
 
 
-    def helper_response(self,page_list, code, hook_url=None):
+#checks if the amount of pages down equals ALL pages, in which case return a different message
+    def are_all_down(self, message, page_list, server = None):
+        pages_down = message.count('\n')+1
+        if pages_down > len(page_list)*.95:
+            if(server is not None):
+                return server + " is down!"
+            else:
+                return "All pages are down!"
+        else:
+            return message
+
+
+    def check_response(self, page_list, code, hook_url=None, server=None, login = False, verify=True):
         test_name = "Model page access "
-        print(str(page_list))
-        assert_error = False
-        response = [requests.get(m, verify=False).status_code for m in page_list]
-        print(response)
+        if login:
+            response = [None for x in range(0,len(page_list))]
+            for x, val in enumerate(page_list):
+                print(val)
+                br = mechanicalsoup.StatefulBrowser(raise_on_404=False)
+                initial_response = br.open(val)
+                if initial_response.status_code < 400:
+                    #login and authenticate
+                    br.select_form('form[name="auth"]')
+                    br["username"] = smoketest_secrets.qed_user
+                    br["password"] = smoketest_secrets.qed_pass
+                    response2 = br.submit_selected()
+                    response[x] = response2.status_code
+                else:
+                    response[x] = initial_response.status_code
+        else:
+            response = [requests.get(m, verify=verify).status_code for m in page_list]
         try:
             assert all(resp == code for resp in response)
                 #assert(npt.assert_array_equal(response, 200, '200 error', True)
@@ -98,82 +144,77 @@ class TestQEDHost(unittest.TestCase):
             codes = [response[i] for i in np.nditer(fail_indices)]
             message = tuple(["Http response failed for: " + str(x) + ". Expecting *" + str(code) + "* but found *" +
                              str(y) + "*." for x, y  in zip(fails,codes)])
-            if(hook_url is not None):
-                self.send_slack_message("\n".join(message), hook_url)
+            slack_message = self.are_all_down("\n".join(message), page_list, server)
+            if hook_url is not None:
+                self.send_slack_message(slack_message, hook_url, server)
             e.args += message
             raise
         return
 
 
-    #public server should return redirects to the login page
-    def test_pub_server_303(self):
-        self.helper_response(pub_model_pages,200, pub_server_hook)
+
+
+    #THE TESTS
+    def test_pub_server_200(self):
+        self.check_response(pub_model_pages,200, smoketest_secrets.pub_server_hook, pub_server, login=True)
         return
 
     def test_internal_s1_200(self):
-        self.helper_response(s1_model_pages,200,s1_hook )
+        self.check_response(s1_model_pages,200,smoketest_secrets.s1_hook, internal_server_1)
         return
 
     def test_interval_s5_200(self):
-        self.helper_response(s5_model_pages,200, s5_hook)
+        self.check_response(s5_model_pages,200, smoketest_secrets.s5_hook, internal_server_5, verify=False)
 
 
 
-    @staticmethod
-    def te_st_qed_404():
-        test_name = "Model page 404 "
+
+    def check_output(self, page_list, code, hook_url=None, server=None, login = False, verify=True):
+        test_name = "Check if model runs and displays output page"
+        if login:
+            response = [None for x in range(0,len(page_list))]
+            for x, val in enumerate(page_list):
+                print(val)
+                br = mechanicalsoup.StatefulBrowser(raise_on_404=False)
+                initial_response = br.open(val)
+                if initial_response.status_code < 400:
+                    #login and authenticate
+                    br.select_form('form[name="auth"]')
+                    br["username"] = smoketest_secrets.qed_user
+                    br["password"] = smoketest_secrets.qed_pass
+                    response2 = br.submit_selected()
+                    response[x] = response2.status_code
+                    br.form = (br.select_form(1))  # select the second form on the page (skipping search bar form)
+                    response3 = br.submit_selected()  # use mechanize to post input data
+                    response3.get_data()
+                    # Verify we have successfully posted input data and that we have arrived at the output page
+                    soup = BeautifulSoup(response3, "html.parser")
+                    tag = [a.find(text=True) for a in soup.findAll('h2', {'class': 'model_header'})]
+                    current_title = val.replace("input", "") + ": " + str(tag[0])
+                    expected_title = val.replace("input", "") + ": " + redirect_models[idx] + " Output"
+                else:
+                    response[x] = "fail"
+        else:
+            response = [requests.get(m, verify=verify).status_code for m in page_list]
         try:
-            assert_error = False
-            response = [requests.get(m) for m in model_pages]
-            check_of404 = ["Ok"] * len(response)
-            assume_no404 = [False] * len(response)  #assume no 404 in page html
-            page_content = [BeautifulSoup(r.content, "html.parser") for r in response]
-            find_N404s = [len(s.findAll('img',src='/static/images/404error.png')) for s in page_content]
-            did_we_find404 = [s>0 for s in find_N404s]
-            for item in did_we_find404:
-                if item == True:
-                    check_of404[item] = "Found 404"
-            try:
-                npt.assert_array_equal(did_we_find404, assume_no404, '404 error', True)
-            except AssertionError:
-                assert_error = True
-            except Exception as e:
-                # handle any other exception
-                print("Error: {}".format(str(e)))
-        except Exception as e:
-            # handle any other exception
-            print("Error: {}".format(str(e)))
-        finally:
-            linkcheck_helper.write_report(test_name, assert_error, model_pages, check_of404)
+            assert all(resp == code for resp in response)
+                #assert(npt.assert_array_equal(response, 200, '200 error', True)
+        except AssertionError as e:
+            fail_indices = np.where(np.array([resp != code for resp in response]))
+            print(fail_indices)
+            fails = [page_list[i] for i in np.nditer(fail_indices)]
+            codes = [response[i] for i in np.nditer(fail_indices)]
+            message = tuple(["Http response failed for: " + str(x) + ". Expecting *" + str(code) + "* but found *" +
+                             str(y) + "*." for x, y  in zip(fails,codes)])
+            slack_message = self.are_all_down("\n".join(message), page_list, server)
+            if hook_url is not None:
+                self.send_slack_message(slack_message, hook_url, server)
+            e.args += message
+            raise
         return
 
-    @staticmethod
-    def te_st_qed_redirect():  #redirects occur on 'input' pages due to login requirement
-        test_name = "Model Input Page Redirect "
-        try:
-            response = [requests.get(m) for m in redirect_model_pages]
-            assert_error = False
-            check_of302 = ["302 Failed"] * len(response)
-            did_we_find302 = [False] * len(response)
-            assume302 = [True] * len(response)  # we're expecting 302 as response history
-            for idx, r in enumerate(response):
-                for resp in r.history:
-                    if resp.status_code == 302:
-                        did_we_find302[idx] = True
-                        check_of302[idx] = "Ok"
-            try:
-                npt.assert_array_equal(did_we_find302, assume302, '302 error', True)
-            except AssertionError:
-                assert_error = True
-            except Exception as e:
-                # handle any other exception
-                print("Error: {}".format(str(e)))
-        except Exception as e:
-            # handle any other exception
-            print("Error: {}".format(str(e)))
-        finally:
-            linkcheck_helper.write_report(test_name, assert_error, redirect_model_pages, check_of302)
-        return
+
+
 
     @staticmethod
     def te_st_qed_authenticate_input():
@@ -206,6 +247,37 @@ class TestQEDHost(unittest.TestCase):
         finally:
             linkcheck_helper.write_report(test_name, assert_error, expected_page, current_page)
         return
+
+
+    def input_form_login(self, page_list, code, hook_url=None, server=None):
+        assert_error = False
+        current_status = [None for x in len(page_list)]
+        expected_status = 200
+        for idx, m in enumerate(page_list):
+            # Create browser object
+            br = mechanicalsoup.StatefulBrowser()
+            br.open(m)
+            #step 1: login and authenticate
+            br.select_form('form[name="auth"]')
+            br["username"] = "qeduser"
+            br["password"] = "EcoDom18!PubServ3"
+            response2 = br.submit_selected()
+            current_status[idx] = response2.status_code
+        try:
+            assert all(status == expected_status for status in expected_status)
+        except AssertionError as e:
+            fail_indices = np.where(np.array([status != code for status in expected_status]))
+            print(fail_indices)
+            fails = [page_list[i] for i in np.nditer(fail_indices)]
+            codes = [expected_status[i] for i in np.nditer(fail_indices)]
+            message = tuple(["QED public - input page inaccessible for: " + str(x) + ". Expecting *" + str(code) + "* but found *" +
+                             str(y) + "*." for x, y in zip(fails, codes)])
+            slack_message = self.are_all_down("\n".join(message), page_list, server)
+            if (hook_url is not None):
+                self.send_slack_message(slack_message, hook_url, server)
+            e.args += message
+            raise
+
 
     @staticmethod
     def te_st_qed_input_form():
