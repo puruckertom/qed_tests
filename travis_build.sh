e42a5688a3920d802fcc7107a3964afd62379514
Tom Purucker <purucker.tom@gmail.com>
2018-07-10 11:32:49 -0500

    adapting travis_build.sh from qed

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-git clone https://github.com/quanted/requirements_qed.git
 
-for package in $(cat requirements_qed/requirements.txt)
+for package in $(cat ./requirements_travis.txt)
 do
     if [[ "$package" == #* ]] || [[ "$package" == *gdal* ]] || [[ "$package" == *fiona* ]] || [[ "$package" == *coverage* ]] || [[ "$package" == *conda* ]]
     then
